eb08292120ec5ac924eafd44aac2105992c70b81
souas <dynamosouras@gmail.com>
2022-11-29 14:56:03 +0530

    added firebase config

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+interface Item {
+  name: string
+};
+
 @Component({
   selector: 'app-folder',
   templateUrl: './folder.page.html',
   styleUrls: ['./folder.page.scss'],
 })
 export class FolderPage implements OnInit {
+
+  item$: Observable<any[]>;
   public folder!: string;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute, firestore: Firestore) {
+    const collection2 = collection(firestore, 'tokenList');
+    this.item$ = collectionData(collection2);
+  }
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'hateq-533d0',
+    appId: '1:77052283990:web:a65da29d4e5f774aca3ff7',
+    storageBucket: 'hateq-533d0.appspot.com',
+    apiKey: 'AIzaSyDLQ-OtfmVXux0d5jkIPvHqNwgfFRexIJM',
+    authDomain: 'hateq-533d0.firebaseapp.com',
+    messagingSenderId: '77052283990',
+    measurementId: 'G-LMSNCQ7BH0',
+  },
   production: true
 };
